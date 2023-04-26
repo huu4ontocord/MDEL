cbd1712ba805e5206fb767dca2064960043114dc
“[Noah <[noahpersaud@zohomail.com]>
2023-04-26 18:19:44 -0400

    Fixed python naming bug in create_uspto

--- scripts/create_uspto_pile_mix.sh
+++ scripts/create_uspto_pile_mix.sh
@@ -4,4 +4,16 @@ DOMAIN_FILE_PATH="../data/pile_uspto/*.jsonl.zst"
 PILE_FILE_PATH="../data/pile_01/01.jsonl.zst"
 OUTPUT_DIR="../data/mix_uspto_all"
 
+
+# Check if python3 command is available and use it if possible
+if command -v python3 &>/dev/null; then
+  PYTHON_CMD=python3
+else
+  PYTHON_CMD=python
+fi
+
+# Run the command using the selected python command
+$PYTHON_CMD -c "from mdel.pile_utils import create_pile_domain_mix; create_pile_domain_mix('$DOMAIN_FILE_PATH', '$PILE_FILE_PATH', '$OUTPUT_DIR')"
+
+
 python -c "from mdel.pile_utils import create_pile_domain_mix; create_pile_domain_mix('$DOMAIN_FILE_PATH', '$PILE_FILE_PATH', '$OUTPUT_DIR')"
