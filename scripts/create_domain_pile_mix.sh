fdb977ba635042e82bdfbc1e2aed29b2f7a65638
Victor May <vmay@chegg.com>
2023-04-28 20:00:49 -0400

    Adding input data sharding

--- scripts/create_domain_pile_mix.sh
+++ scripts/create_domain_pile_mix.sh
@@ -7,9 +7,15 @@ else
   PYTHON_CMD=python
 fi
 
+SUBSET_NAME="USPTO Backgrounds"
+
 for SPLIT in "test" "val" "train"
 do
   PILE_FILE_PATH="../data/pile/$SPLIT/*.jsonl.zst"
   OUTPUT_DIR="../data/mix_uspto_all/$SPLIT"
-  $PYTHON_CMD -c "from mdel.pile_utils import *; create_pile_domain_mix('$PILE_FILE_PATH', '$PILE_FILE_PATH', '$OUTPUT_DIR')"
+
+  # Shard the input data
+  $PYTHON_CMD -c "from mdel.pile_utils import *; split_pile('$PILE_FILE_PATH')"
+
+  $PYTHON_CMD -c "from mdel.pile_utils import *; create_pile_domain_mix('$PILE_FILE_PATH', '$PILE_FILE_PATH', '$OUTPUT_DIR', '$SUBSET_NAME')"
 done
