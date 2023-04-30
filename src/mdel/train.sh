1bd43bd9aca20d98653e75df10012d82cf55ecfd
mrcabbage972 <mayvic@gmail.com>
2023-04-30 18:18:03 -0400

    Adding push to hub

--- src/mdel/train.sh
+++ src/mdel/train.sh
@@ -11,7 +11,7 @@ accelerate launch trainer.py \
         --output_dir "ckpts/pythia-1b-deduped/$DATASET/layer_$TRAINING_LAYER" \
         --training_layers $TRAINING_LAYER \
         --per_device_train_batch_size 1 \
-        --per_device_eval_batch_size 1 \
+        --per_device_eval_batch_size 8 \
         --preprocessing_num_workers 32 \
         --learning_rate 1e-4 \
         --block_size 512 \
@@ -23,4 +23,7 @@ accelerate launch trainer.py \
         --eval_steps 200 \
         --overwrite_output_dir \
         --logging_steps 20 \
-        --max_steps 1000
+        --max_steps 1000 \
+        --push_to_hub true \
+        --push_to_hub_model_id expert-$DATASET \
+        --push_to_hub_organization Multi-Domain-Expert-Layers
