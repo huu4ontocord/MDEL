a6654c4bfccc7bd2c93e760bfe9ce6f695fb27db
mrcabbage972 <mayvic@gmail.com>
2023-05-01 20:36:55 -0400

    Incorporating review comments

--- src/mdel/train.sh
+++ src/mdel/train.sh
@@ -1,29 +1,25 @@
 #!/bin/bash
 
 DATASET=uspto
-TRAINING_LAYER=9,10,11,12,13
+TRAINING_LAYERS=9,10,11,12,13
 
 export WANDB_PROJECT=pythia-1b-deduped-layer-test-$DATASET
-export WANDB_NAME="layer_$TRAINING_LAYER"
+export WANDB_NAME="layer_$TRAINING_LAYERS"
 export WANDB_ENTITY=ontocord
 
-VENV_DIR="../../venv"
-if [ -d "$VENV_DIR" ]; then
-    echo "Virtual environment exists."
+# check if venv or conda is activated
+if [ -n "$CONDA_DEFAULT_ENV" ] || [ -n "$VIRTUAL_ENV" ]; then
+    echo "Virtual environment is activated"
 else
-    echo "Error: virtual environment is missing"
+    echo "Error: virtual environment is not activated"
     exit 1
 fi
-
-# Activate the venv
-echo "Activating virtual environment..."
-source $VENV_DIR/bin/activate
 
 accelerate launch trainer.py \
         --dataset_name Multi-Domain-Expert-Layers/$DATASET \
         --model_name_or_path EleutherAI/pythia-1b-deduped \
-        --output_dir "ckpts/pythia-1b-deduped/$DATASET/layer_$TRAINING_LAYER" \
-        --training_layers $TRAINING_LAYER \
+        --output_dir "ckpts/pythia-1b-deduped/$DATASET/layer_$TRAINING_LAYERS" \
+        --training_layers $TRAINING_LAYERS \
         --per_device_train_batch_size 1 \
         --per_device_eval_batch_size 8 \
         --preprocessing_num_workers 32 \
@@ -41,6 +37,3 @@ accelerate launch trainer.py \
         --push_to_hub true \
         --push_to_hub_model_id expert-$DATASET \
         --push_to_hub_organization Multi-Domain-Expert-Layers
-
-echo "Deactivating virtual environment..."
-deactivate
