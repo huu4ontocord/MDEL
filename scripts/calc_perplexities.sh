3d653e509e18d33e1e5746cfc8c0c79e910653a8
Jason Stillerman <jason.t.stillerman@gmail.com>
2023-05-15 08:20:59 -0400

    calc perplex job

---
+++ scripts/calc_perplexities.sh
@@ -0,0 +1,16 @@
+if command -v python3 &>/dev/null; then
+  PYTHON_CMD=python3
+else
+  PYTHON_CMD=python
+fi
+
+SUBSET_NAME="USPTO Backgrounds"
+
+for MODEL in "Multi-Domain-Expert-Layers/expert-arxiv" "Multi-Domain-Expert-Layers/expert-freelaw" "Multi-Domain-Expert-Layers/expert-github"
+do
+  for DATASET in "Multi-Domain-Expert-Layers/arxiv" "Multi-Domain-Expert-Layers/freelaw" "Multi-Domain-Expert-Layers/github"
+  do
+
+    echo $PYTHON_CMD ../src/mdel/calculate_perplexity.py --model $MODEL --dataset $DATASET --split validation_domain
+  done 
+done
