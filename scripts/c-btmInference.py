47314fb5d9a4cd9a6997eff30d6180bc4feb6043
NourFahmy <m.fahmynour@gmail.com>
2023-09-03 15:37:25 -0400

    updated c-btm  inference script
    pass in embedder for prompt

--- scripts/c-btmInference.py
+++ scripts/c-btmInference.py
@@ -1,6 +1,6 @@
 from transformers import AutoModelForCausalLM, AutoTokenizer
 import torch
-from torch import torch.nn.PairwiseDistance
+from torch.nn import PairwiseDistance
 import numpy 
 
 def load_models(model_names):
@@ -12,7 +12,7 @@ def load_models(model_names):
     tokenizers = []
 
     for model_name in model_names:
-        model = AutoModel.from_pretrained(model_name)
+        model = AutoModelForCausalLM.from_pretrained(model_name)
         tokenizer = AutoTokenizer.from_pretrained(model_name)
 
         # Freeze the parameters of the loaded models if needed
@@ -32,30 +32,31 @@ def generateNextTokenFromExpert(prompt, tokenizer, model):
   returns the predicted token of the model and its corresponding probability
   """
   inputs = tokenizer(prompt)
-  sizeOfInputs = inputs['input_ids'].size()[1]
+  print(inputs['input_ids'])
+  sizeOfInputs = len(inputs['input_ids']) 
   outputs = model(**inputs, max_new_tokens=1, 
                   return_dict_in_generate=True,
                   output_scores=True
                   )
   predToken = outputs['sequences'][0][sizeOfInputs] # pull out first token only
   predTokenProb = outputs['scores'][0][0]['predToken']
- 
+
   return predToken, predTokenProb
 
-def findEnsembleWeights(prompt, clusterCenters, T):
+def findEnsembleWeights(embedder, prompt, T, clusterCenters):
   """
   takes in embedding of context and cluster center of domain, and T parameter 
   calculates weighted logit between context and cluster center
   """
   ensembleWeights = []
   pdist = torch.nn.PairwiseDistance(p=2)
-
   for domain, clusterCenter in enumerate(clusterCenters): # assuming modelList and clusterCenters have matching indices
-    tokenizer = tokenizers[domain]
-    tokenizedInput = tokenizer(prompt)
-    ensembleWeights.append(torch.exp(-1 * torch.pow(pdist(tokenizedInput, clusterCenter),2) / T))
-  
-  return ensembleWeights
+    embeddedInput = embedder.encode(prompt) 
+    clusterCenter = torch.tensor(clusterCenter)
+    embeddedInput = torch.tensor(embeddedInput) 
+    ensembleWeights.append(torch.exp(-1 * torch.pow(pdist(embeddedInput, clusterCenter),2) / T))
+
+  return torch.tensor(ensembleWeights) 
 
 
 
@@ -65,29 +66,32 @@ def topKFilter(ensembleWeights, k):
   returns top k ensemble weights to determine most relevant domains given context
   """
   topK = torch.topk(ensembleWeights, k=k)
-  topK = [float(p)/torch.sum(topk.values) for p in topk.values]
-  
-  return topK
+  indices = topK.indices
+  topK = [float(p)/torch.sum(topK.values) for p in topK.values]
 
-def findNextToken(prompt, k, T, clusterCenters):
+  return topK, indices
+
+def findNextToken(embedder, models, tokenizers, prompt, k, T, clusterCenters):
   """
   takes in prompt, temperature T parameter and clusterCenters
   finds k most relevant domains given context
   returns most likely next token from predictions of most relevant domain experts
   """
-  ensembleWeights = findEnsembleWeights(prompt, T, clusterCenters)
-  topK = topKFilter(ensembleWeights, k)
+  ensembleWeights = findEnsembleWeights(embedder, prompt, T, clusterCenters)
+  topKValues, topKIndices = topKFilter(ensembleWeights, k)
   expertsPredictedTokens = []
-  for index in topK.indices:
-    predToken, predTokenProb = generateNextTokenFromExpert(prompt,
+  for i, index in enumerate(topKIndices):
+    predToken, predTokenProb = generateNextTokenFromExpert(
+                                                           prompt,
                                                            tokenizers[index],
-                                                           models[index])
-    predTokenProb *= topK.values[index]
+                                                           models[index]
+                                                           )
+    predTokenProb *= topKValues[i]
     expertsPredictedTokens.append((predToken, predTokenProb.numpy())) # convert to regular number for max
 
   return max(expertsPredictedTokens, key=lambda x: x[1])
 
-def generateSequence(prompt, end_token, maxLength, k, T, clusterCenters):
+def generateSequence(embedder, prompt, end_token, models, tokenizers, maxLength, k, T, clusterCenters):
   """
   takes in prompt, end_token which ideally is uniform across all tokenizers, 
   parameter k, temperature T and cluster centers
@@ -95,24 +99,23 @@ def generateSequence(prompt, end_token, maxLength, k, T, clusterCenters):
   builds sequence until end token is generated or maxLength is reached
   """
   currToken, currSequence = None, prompt
-
   while currToken != end_token or len(currSequence) < maxLength:
-    currToken = findNextToken(k, T, clusterCenters)
+    currToken = findNextToken(embedder, models, tokenizers, prompt, k, T, clusterCenters)
     currSequence = torch.cat((currSequence, currToken), dim=0)
 
   return generateSequence
 
 
-def run_inference(model_names, input_file, output_file, end_token, maxLength, k, T, clusterCenters):
-    
-    models, tokenizers = load_models(model_names)
+def run_inference(embedder, model_names, input_file, output_file, end_token, maxLength, k, T, clusterCenters):
+
+    models, tokenizers = load_models(model_names) # TODO: fails to recognize models even with credentials
 
     with open(input_file, 'r') as file:
         input_data = file.readlines()
 
     results = []
     for prompt in input_data:
-        results.append(generateSequence(prompt, end_token, maxLength, k, T, clusterCenters))
+        results.append(generateSequence(embedder, prompt, end_token, models, tokenizers, maxLength, k, T, clusterCenters))
 
     with open(output_file, 'w') as file:
         for result in results:
